777405bd2f3799123c2c0896c8bbb3421ba090b8
Xavier <xavieralves162@gmail.com>
2026-02-03 16:45:56 +0000

    Exc 4.5

--- bloglist-backend/tests/list_helper.test.js
+++ bloglist-backend/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
 const listHelper = require('../utils/list_helper')
@@ -8,4 +8,22 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
 })
