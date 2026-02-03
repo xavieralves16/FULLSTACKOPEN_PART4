aae1874ebf4ffc7922770004bc524b7bddd64e29
Xavier <xavieralves162@gmail.com>
2026-02-03 16:48:47 +0000

    Exc 4.5

--- bloglist-backend/tests/list_helper.test.js
+++ bloglist-backend/tests/list_helper.test.js
@@ -27,3 +27,35 @@ describe('total likes', () => {
     assert.strictEqual(result, 5)
   })
 })
+
+describe('favorite blog', () => {
+  const blogs = [
+    {
+      _id: '1',
+      title: 'First blog',
+      author: 'Alice',
+      url: 'http://example.com/1',
+      likes: 5,
+    },
+    {
+      _id: '2',
+      title: 'Second blog',
+      author: 'Bob',
+      url: 'http://example.com/2',
+      likes: 12,
+    },
+    {
+      _id: '3',
+      title: 'Third blog',
+      author: 'Carol',
+      url: 'http://example.com/3',
+      likes: 8,
+    }
+  ]
+
+  test('returns the blog with the most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+
+    assert.deepStrictEqual(result, blogs[1])
+  })
+})
--- bloglist-backend/utils/list_helper.js
+++ bloglist-backend/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite
+  })
+}
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
