dfbb9c6ee4c2fe6f85f63ffb039bde271c4864c8
Xavier <xavieralves162@gmail.com>
2026-02-05 08:08:07 +0000

    Exc 4.2

--- bloglist-backend/tests/blog_api.test.js
+++ bloglist-backend/tests/blog_api.test.js
@@ -37,6 +37,15 @@ test('blogs are returned as JSON and the correct amount', async () => {
   assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
+test('blog posts have id property instead of _id', async () => {
+  const response = await api.get('/api/blogs')
+
+  const blog = response.body[0]
+
+  assert(blog.id)
+  assert.strictEqual(blog._id, undefined)
+})
+
 
 after(async () => {
   await mongoose.connection.close()
